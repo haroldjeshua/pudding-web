e7a598e96915b6e45f6313f25bb6b527f087c14c
Harold Jeshua Varde <harvardii15@gmail.com>
2021-09-29 17:17:56 +0800

    update 517 092921

--- js/main.js
+++ js/main.js
@@ -360,6 +360,23 @@
         });
     };
 
+    var clDropdown = function() {
+        document.addEventListener("click", e => {
+            const isDropdownButton = e.target.matches("[data-dropdown-button]")
+            if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+          
+            let currentDropdown
+            if (isDropdownButton) {
+              currentDropdown = e.target.closest("[data-dropdown]")
+              currentDropdown.classList.toggle("active")
+            }
+          
+            document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+              if (dropdown === currentDropdown) return
+              dropdown.classList.remove("active")
+            })
+          })
+    };
 
    /* Cookie Control
     * ------------------------------------------------------ */
@@ -391,6 +408,7 @@
         clAjaxChimp();
         clTilt();
         // cookieCtrl();
+        clDropdown();
 
     })();
 
